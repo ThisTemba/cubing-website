1dd8430a0e4221d7acbb00f004829998e59d3488
ThisTemba <36087610+ThisTemba@users.noreply.github.com>
2021-07-16 14:12:40 -0400

    Simplify and style Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,23 @@
 import React from "react";
-import {
-  Navbar as NavbarRB,
-  Nav,
-  NavDropdown,
-  Form,
-  FormControl,
-  Button,
-} from "react-bootstrap";
+import { Navbar as NavbarRB, Nav, NavDropdown } from "react-bootstrap";
 
 export default function Navbar() {
   return (
-    <NavbarRB bg="light" expand="lg">
-      <NavbarRB.Brand href="#home">React-Bootstrap</NavbarRB.Brand>
+    <NavbarRB bg="dark" variant="dark" expand="md">
+      <NavbarRB.Brand href="#home">GCW</NavbarRB.Brand>
       <NavbarRB.Toggle aria-controls="basic-navbar-nav" />
       <NavbarRB.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
+          <Nav.Link href="#home">Time</Nav.Link>
+          <Nav.Link href="#link">Train</Nav.Link>
           <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1">Account</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Log Out</NavDropdown.Item>
           </NavDropdown>
         </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-success">Search</Button>
-        </Form>
+        <Nav>
+          <Nav.Link href="#home">Log Out</Nav.Link>
+        </Nav>
       </NavbarRB.Collapse>
     </NavbarRB>
   );
