0b35a2e77ce6aadc7e0195bea1c6b9d69e192fbb
ThisTemba <36087610+ThisTemba@users.noreply.github.com>
2021-07-16 14:41:02 -0400

    Add Link to Brand in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Navbar as NavbarRB, Nav, NavDropdown } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <NavbarRB bg="dark" variant="dark" expand="md">
-      <NavbarRB.Brand to="/">GCW</NavbarRB.Brand>
+      <NavbarRB.Brand as={Link} to="/">
+        GCW
+      </NavbarRB.Brand>
       <NavbarRB.Toggle aria-controls="basic-navbar-nav" />
       <NavbarRB.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
