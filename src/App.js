92e62c73b7ae009505e7ea20e3f4c67cd7439549
ThisTemba <36087610+ThisTemba@users.noreply.github.com>
2021-07-16 14:29:35 -0400

    Add basic routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import TrainPage from "./components/trainPage";
+import TimePage from "./components/timePage";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Route path="/time" component={TimePage} />
+      <Route path="/train" component={TrainPage} />
     </div>
   );
 }
