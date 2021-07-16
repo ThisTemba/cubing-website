98749830827d5d6160f88c93666a87ebfdf5e695
ThisTemba <36087610+ThisTemba@users.noreply.github.com>
2021-07-16 14:14:35 -0400

    Add timePage and trainPage

---
+++ src/components/timePage.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function TimePage() {
+  return <h1>Time Page</h1>;
+}
---
+++ src/components/trainPage.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function TrainPage() {
+  return <h1>Train Page</h1>;
+}
