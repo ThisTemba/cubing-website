1c2d3f41f893ce4fb12de958b5209bacf8057fe8
ThisTemba <36087610+ThisTemba@users.noreply.github.com>
2021-07-16 14:32:02 -0400

    Connect Navlinks to routes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,27 @@
 import React from "react";
 import { Navbar as NavbarRB, Nav, NavDropdown } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <NavbarRB bg="dark" variant="dark" expand="md">
-      <NavbarRB.Brand href="#home">GCW</NavbarRB.Brand>
+      <NavbarRB.Brand to="/">GCW</NavbarRB.Brand>
       <NavbarRB.Toggle aria-controls="basic-navbar-nav" />
       <NavbarRB.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="#home">Time</Nav.Link>
-          <Nav.Link href="#link">Train</Nav.Link>
+          <Nav.Link as={NavLink} to="/time">
+            Time
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/train">
+            Train
+          </Nav.Link>
           <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Account</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Log Out</NavDropdown.Item>
+            <NavDropdown.Item to="#action/3.1">Account</NavDropdown.Item>
+            <NavDropdown.Item to="#action/3.3">Log Out</NavDropdown.Item>
           </NavDropdown>
         </Nav>
         <Nav>
-          <Nav.Link href="#home">Log Out</Nav.Link>
+          <Nav.Link to="#home">Log Out</Nav.Link>
         </Nav>
       </NavbarRB.Collapse>
     </NavbarRB>
