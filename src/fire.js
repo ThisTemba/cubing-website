56a71025ba4116c223e0e6d544899fa7e15faa1c
Temba Mateke <tembamateke@gmail.com>
2021-07-16 22:23:22 -0400

    Add fire.js file
    This is where all the firebase initialization stuff will live
    This is what you import if you want to use firebase
    Though I should also probably put these in environment variables
    Can do later

---
+++ src/fire.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import firebase from "firebase";
+import "firebase/auth";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyD3KDYppKymwXOQcffJdWI_3bZzLsaiUGE",
+  authDomain: "cubing-website.firebaseapp.com",
+  projectId: "cubing-website",
+  storageBucket: "cubing-website.appspot.com",
+  messagingSenderId: "2145506796",
+  appId: "1:2145506796:web:1e8b26a481cfb66d51dcc9",
+  measurementId: "G-BYLSDMPCQ0",
+};
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+
+export const useAuthState = () => {
+  const [user, setUser] = useState(null);
+  firebase.auth().onAuthStateChanged((user) => {
+    setUser(user ? user : null);
+  });
+  return user;
+};
