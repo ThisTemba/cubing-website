8c411a02ab1450914a7b4563d4fe3ea3956edf98
Temba Mateke <tembamateke@gmail.com>
2021-07-18 10:27:56 -0400

    Add basic session functionality
    Users want to save their solves
    I want to save them in Firebase's Firestore database
    Firestore doesn't like a lot of unnecessary document writes and reads
    Better to have solves grouped into sessions
    This is a basic implementation of sessions

--- src/components/timePage.jsx
+++ src/components/timePage.jsx
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Timer from "./common/timer";
+import { Button } from "react-bootstrap";
 
 export default function TimePage() {
+  const [session, setSession] = useState();
+
+  const puzzle = "333";
+
+  useEffect(() => {
+    handleNewSession();
+  }, []);
+
+  const handleNewSession = () => {
+    // save current session to cloud
+    console.log(session);
+
+    const dateTime = new Date();
+    setSession({
+      name: dateTime.toLocaleDateString() + " " + dateTime.toLocaleTimeString(),
+      date: dateTime.toLocaleDateString(),
+      dateTime: dateTime,
+      puzzle: puzzle,
+      solves: [],
+    });
+
+    document.activeElement.blur(); // remove focus from new session button
+    // because if you don't do this, pressing space afterwards triggers the button
+  };
+
   return (
     <div>
       <h1>Time Page</h1>
-      <Timer />
+      <Timer
+        onNewSolve={(solve) => {
+          session.solves.push(solve);
+        }}
+      />
+      <Button onClick={handleNewSession}>New Session</Button>
     </div>
   );
 }
