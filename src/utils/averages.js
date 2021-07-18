5921241280d97ef31cfd4468c071b8830e7e4d93
Temba Mateke <tembamateke@gmail.com>
2021-07-18 17:27:58 -0400

    Add averages util for aoN calculations
    The aoN function can be used for ao5, ao12, ao50, ao100 etc.

---
+++ src/utils/averages.js
@@ -0,0 +1,31 @@
+import _ from "lodash";
+
+export const aoN = (Nsolves, N) => {
+  if (Nsolves.length !== N) {
+    throw "Nsolves.length !== N in aoN calculation";
+  }
+  let sortedSolves = DNFsort(Nsolves);
+  const excludeNum = Math.ceil(N * 0.05);
+  console.log(excludeNum);
+
+  let trimmedSolves = _.slice(
+    sortedSolves,
+    excludeNum,
+    sortedSolves.length - excludeNum
+  );
+
+  if (_.some(trimmedSolves, ["penalty", "DNF"])) {
+    return "DNF";
+  } else {
+    return _.mean(trimmedSolves.map((s) => s.solveTime.timeSeconds));
+  }
+};
+
+export const DNFsort = (solves) => {
+  let DNFsolves = _.groupBy(solves, "penalty")["DNF"];
+  let nonDNFsolves = _.difference(solves, DNFsolves);
+  DNFsolves = _.sortBy(DNFsolves, "solveTime.timeSeconds"); // why not
+  nonDNFsolves = _.sortBy(nonDNFsolves, "solveTime.timeSeconds");
+  const sortedSolves = _.concat(nonDNFsolves, DNFsolves);
+  return sortedSolves;
+};
