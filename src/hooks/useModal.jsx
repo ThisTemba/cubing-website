964b92b4d59a3406d6298841d73fb4bcbe8777f9
Temba Mateke <tembamateke@gmail.com>
2021-07-20 22:12:09 -0400

    Add useModal hook
    As it is now, the way it works is you put the
    ModalComponent somewhere in the page where you want it to render
    Then you call showModal and pass the modal content and the modal pops up
    This can be extended to include custom buttons,
    but for now, the only button is a close button

---
+++ src/hooks/useModal.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+
+export default function useModal() {
+  const [show, setShow] = useState(false);
+  const [content, setContent] = useState({
+    title: "Modal Title",
+    body: "Modal Body",
+  });
+  const showModal = (content) => {
+    setContent(content);
+    setShow(true);
+  };
+  const ModalComponent = () => {
+    return (
+      <Modal show={show} onHide={() => setShow(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>{content.title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{content.body}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShow(false)}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  };
+  return [ModalComponent, showModal];
+}
