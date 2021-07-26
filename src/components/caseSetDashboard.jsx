d127cd4a56b6855334f2fff3db113444aa0ddbbc
Temba Mateke <tembamateke@gmail.com>
2021-07-26 15:56:20 -0400

    Use sr-visualizer CubeImage

--- src/components/caseSetDashboard.jsx
+++ src/components/caseSetDashboard.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import CaseSetCard from "./common/cubing/caseSetCard";
-import { Row, Col, Container } from "react-bootstrap";
+import { Row } from "react-bootstrap";
 
 export default function CaseSetDashboard() {
   return (
     <div>
       <h1>Case Set Dashboard</h1>
       <Row className="">
-        <CaseSetCard alg1="r U R' U R U2 r'" stage1="oll" stage2="oll" />
+        <CaseSetCard case1="r U R' U R U2 r'" mask1="oll" mask2="oll" />
         <CaseSetCard
-          alg1="R' U' R U D' R2 U R' U R U' R U' R2 D"
-          stage1="ll"
-          stage2="ll"
+          case1="R' U' R U D' R2 U R' U R U' R U' R2 D"
+          mask1="ll"
+          mask2="ll"
         />
       </Row>
     </div>
--- src/components/common/cubing/caseSetCard.jsx
+++ src/components/common/cubing/caseSetCard.jsx
@@ -5,7 +5,7 @@ import Button from "react-bootstrap/Button";
 import { Row, Col } from "react-bootstrap";
 
 export default function CaseSetCard(props) {
-  const { alg1, stage1, stage2 } = props;
+  const { case1, mask1, mask2 } = props;
   return (
     <Col className="d-flex justify-content-center">
       <Card style={{ width: "500px" }} className="m-1">
@@ -13,13 +13,13 @@ export default function CaseSetCard(props) {
           <Card.Body>
             <Row className="align-items-center">
               <Col>
-                <CubeImage alg={alg1} stage={stage1} />
+                <CubeImage case={case1} mask={mask1} />
               </Col>
               <Col>
                 <i class="fa fa-long-arrow-right fa-4x" aria-hidden="true"></i>
               </Col>
               <Col>
-                <CubeImage stage={stage2} />
+                <CubeImage mask={mask2} />
               </Col>
             </Row>
             <Card.Title>{props.title}</Card.Title>
