fca73639811b74d1cc5bd406bd6dc1e5d7f3ebed
Temba Mateke <tembamateke@gmail.com>
2021-07-27 19:48:42 -0400

    Add caseSets.test to verify case set structure
    Case sets need a name, mask, and view, among other things
    Cases need an id, name, group, algs and scrambles
    The group here refers to a visual group or recognition group
    It is the group of cases which are likely to be mistaken for one another
    Probably

---
+++ src/data/caseSets.test.js
@@ -0,0 +1,50 @@
+// import ollCaseSet from "./ollCaseSet";
+import pllCaseSet from "./pllCaseSet";
+import ollCaseSet from "./ollCaseSet";
+import _ from "lodash";
+
+const caseSets = [ollCaseSet, pllCaseSet];
+const caseSetKeys = ["details", "cases"];
+const detailsKeys = ["name", "mask", "view", "numCases"];
+const caseKeys = ["id", "name", "group", "scrambles", "algs"];
+
+const testForKeys = (object, keys) => {
+  keys.forEach((key) => {
+    it(`has ${key} property`, () => {
+      expect(object[key]).toBeDefined();
+    });
+  });
+};
+
+caseSets.forEach((caseSet) => {
+  describe(caseSet.details.name, () => {
+    testForKeys(caseSet, caseSetKeys);
+
+    test("cases isn't empty", () => {
+      expect(caseSet.cases.length > 0);
+    });
+
+    describe("details", () => {
+      const details = caseSet.details;
+      testForKeys(details, detailsKeys);
+
+      test("numCases is accurate", () => {
+        const { numCases } = details;
+        const { cases } = caseSet;
+        expect(numCases).toBe(cases.length);
+      });
+    });
+
+    describe("random case", () => {
+      const cas = _.sample(caseSet.cases);
+      testForKeys(cas, caseKeys);
+
+      it("scrambles has at least four scrambles", () => {
+        expect(cas.scrambles.length >= 4).toBeTruthy();
+      });
+      it("algs isn't empty", () => {
+        expect(cas.algs.length > 0).toBeTruthy();
+      });
+    });
+  });
+});
