1bc5aa0508ec5d67a8c49f394a34aa8db43e3fed
Temba Mateke <tembamateke@gmail.com>
2021-07-29 10:27:12 -0400

    Add TestPage route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import TrainPage from "./components/pages/trainPage";
 import TimePage from "./components/pages/timePage";
 import StatsPage from "./components/pages/statsPage";
+import TestPage from "./components/pages/testPage";
 import PasswordReset from "./components/passwordReset";
 import SignUp from "./components/signUp";
 import LogIn from "./components/logIn";
@@ -13,6 +14,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Switch>
+        <Route path="/train/test" component={TestPage} />
         <Route path="/time" component={TimePage} />
         <Route path="/train" component={TrainPage} />
         <Route path="/stats" component={StatsPage} />
