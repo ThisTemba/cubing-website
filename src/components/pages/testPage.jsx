4a52f8881fe0a01967a6858d08639513447ce599
Temba Mateke <tembamateke@gmail.com>
2021-07-29 12:12:02 -0400

    Test ReactTable on TestPage

--- src/components/pages/testPage.jsx
+++ src/components/pages/testPage.jsx
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useMemo } from "react";
+import ReactTable from "../common/reactTable";
+import { Container } from "react-bootstrap";
+import { useTable, useSortBy } from "react-table";
 
 export default function TestPage() {
+  const data = useMemo(
+    () => [
+      {
+        name: "Tim",
+        age: "About 50",
+      },
+      {
+        name: "Art",
+        age: "About 25",
+      },
+    ],
+    []
+  );
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: "Name",
+        accessor: "name",
+      },
+      {
+        Header: "Age",
+        accessor: "age",
+      },
+    ],
+    []
+  );
+
+  const table = useTable({ columns, data });
+
   return (
-    <div>
+    <Container>
       <h1>Test Page</h1>
-    </div>
+      <ReactTable table={table} />
+    </Container>
   );
 }
