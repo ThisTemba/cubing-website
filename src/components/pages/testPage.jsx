30a0927d1975f953158c914eed7fdd847399c595
Temba Mateke <tembamateke@gmail.com>
2021-07-29 13:46:38 -0400

    Test grouping with expansion

--- src/components/pages/testPage.jsx
+++ src/components/pages/testPage.jsx
@@ -1,7 +1,7 @@
 import React, { useMemo } from "react";
 import ReactTable from "../common/reactTable";
 import { Container } from "react-bootstrap";
-import { useTable, useSortBy } from "react-table";
+import { useTable, useSortBy, useGroupBy, useExpanded } from "react-table";
 
 export default function TestPage() {
   const data = useMemo(
@@ -32,7 +32,8 @@ export default function TestPage() {
     []
   );
 
-  const table = useTable({ columns, data });
+  // const table = useTable({ columns, data });
+  const table = useTable({ columns, data }, useGroupBy, useSortBy, useExpanded);
 
   return (
     <Container>
