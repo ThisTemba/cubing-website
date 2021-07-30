ff270545f609dc269adfd9866d6d73120b30322b
Temba Mateke <tembamateke@gmail.com>
2021-07-30 17:23:29 -0400

    Add back to dash button

--- src/components/pages/learnPage.jsx
+++ src/components/pages/learnPage.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 
-export default function LearnPage() {
+export default function LearnPage(props) {
   return (
     <div>
       <h1>Learn Page</h1>
+      <Button
+        onClick={() => {
+          props.history.push("/train");
+          props.onDashboard();
+        }}
+        variant="secondary"
+        className="m-1"
+      >
+        <i className="fa fa-chevron-left" aria-hidden="true"></i> Back to
+        Dashboard
+      </Button>
     </div>
   );
 }
