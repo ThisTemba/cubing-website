056c62889454d80084a6a527ef99da7856d1a1f4
Temba Mateke <tembamateke@gmail.com>
2021-07-30 13:46:47 -0400

    Remove react-table test from TestPage

--- src/components/pages/testPage.jsx
+++ src/components/pages/testPage.jsx
@@ -1,44 +1,10 @@
-import React, { useMemo } from "react";
-import ReactTable from "../common/reactTable";
+import React from "react";
 import { Container } from "react-bootstrap";
-import { useTable, useSortBy, useGroupBy, useExpanded } from "react-table";
 
 export default function TestPage() {
-  const data = useMemo(
-    () => [
-      {
-        name: "Tim",
-        age: "About 50",
-      },
-      {
-        name: "Art",
-        age: "About 25",
-      },
-    ],
-    []
-  );
-
-  const columns = useMemo(
-    () => [
-      {
-        Header: "Name",
-        accessor: "name",
-      },
-      {
-        Header: "Age",
-        accessor: "age",
-      },
-    ],
-    []
-  );
-
-  // const table = useTable({ columns, data });
-  const table = useTable({ columns, data }, useGroupBy, useSortBy, useExpanded);
-
   return (
     <Container>
       <h1>Test Page</h1>
-      <ReactTable table={table} />
     </Container>
   );
 }
