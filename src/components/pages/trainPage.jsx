261793ca15c8fbe1a7477409beb1679dedd6718d
Temba Mateke <tembamateke@gmail.com>
2021-07-30 17:04:33 -0400

    Put Case Set Dashboard in 'Fake Route'
    Really weird bug where I try to render this inside a route
    while at the same time calling setSelectedCases and setting
    it to a local state here and the thing gets stuck in an infinite loop
    this is a "fake route" that works using display dashboard which should
    suffice for now

--- src/components/pages/trainPage.jsx
+++ src/components/pages/trainPage.jsx
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import CaseSetDashboard from "../caseSetDashboard";
 
-export default function TrainPage() {
+export default function TrainPage(props) {
+  const [selectedCases, setSelectedCases] = useState();
+  const [displayDashboard, setDisplayDashboard] = useState(true);
+  const handleNewSelection = (params) => {
+    setSelectedCases(params);
+  };
+  const handleTest = (params) => {
+    console.log(params, "train");
+    props.history.push("/train/test");
+    setDisplayDashboard(false);
+  };
+  const handleLearn = (params) => {
+    console.log(params, "learn");
+    props.history.push("/train/learn");
+    setDisplayDashboard(false);
+  };
+
+  useEffect(() => {
+    console.table(selectedCases);
+  }, [selectedCases]);
+
   return (
     <Container>
-      <CaseSetDashboard />
+      {displayDashboard && (
+        <CaseSetDashboard
+          setSelectedCases={handleNewSelection}
+          onTest={handleTest}
+          onLearn={handleLearn}
+        />
+        // Really weird bug where I try to render this inside a route
+        // while at the same time calling setSelectedCases and setting
+        // it to a local state here and the thing gets stuck in an infinite loop
+        // this is a "fake route" that works using display dashboard which should
+        // suffice for now
+      )}
     </Container>
   );
 }
