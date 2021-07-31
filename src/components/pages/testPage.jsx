f5f8e4e42c30a1c0502d07ed41b18e243e5608d4
Temba Mateke <tembamateke@gmail.com>
2021-07-31 12:05:09 -0400

    Extract prepareCaseData to caseStats util

--- src/components/pages/testPage.jsx
+++ src/components/pages/testPage.jsx
@@ -8,9 +8,7 @@ import ButtonGroupToggle from "../common/buttonGroupToggle";
 import { displayDur } from "../../utils/formatTime";
 import { db } from "../../fire";
 import { useAuthState } from "../../fire";
-
-const CASE_SOLVES_CAP = 10;
-const CASE_SOLVES_STAT_CAP = 3;
+import { prepareCaseData } from "../../utils/caseStats";
 
 export default function TestPage(props) {
   const { selectedCases, caseSetDetails } = props;
@@ -128,44 +126,6 @@ export default function TestPage(props) {
     document.activeElement.blur();
   };
 
-  const prepareCaseData = (caseId, oldDoc) => {
-    let newSolves = _.filter(solves, ["caseId", caseId]);
-
-    let oldSolves = [];
-    let numSolves = newSolves.length;
-
-    if (oldDoc.exists) {
-      oldSolves = oldDoc.data().recentCaseSolves;
-      numSolves += oldDoc.data().caseStats.numSolves;
-    }
-    let allSolves = [...newSolves, ...oldSolves];
-
-    // num case solves to store: CASE_SOLVES_CAP
-    let recentCaseSolves = _.take(allSolves, CASE_SOLVES_CAP);
-
-    // num case solves to calculate stats from: CASE_SOLVES_STAT_CAP
-    let statCaseSolves = _.take(recentCaseSolves, CASE_SOLVES_STAT_CAP);
-    let hRate = statCaseSolves.filter((s) => s.hesitated === true);
-    let mmRate = statCaseSolves.filter((s) => s.mistakes === 1);
-    let cmRate = statCaseSolves.filter((s) => s.mistakes === 2);
-    hRate = hRate.length / statCaseSolves.length;
-    mmRate = mmRate.length / statCaseSolves.length;
-    cmRate = cmRate.length / statCaseSolves.length;
-    const avgTime = _.mean(statCaseSolves.map((s) => s.dur));
-    const caseStats = {
-      numSolves,
-      hRate,
-      mmRate,
-      cmRate,
-      avgTime,
-    };
-
-    const data = { caseStats, recentCaseSolves };
-    console.log("preparedData", data);
-
-    return data;
-  };
-
   const writeCaseToFirebase = (caseId, data) => {
     getCaseDocRef(caseId)
       .set(data)
@@ -182,7 +142,7 @@ export default function TestPage(props) {
       getCaseDocRef(caseId)
         .get()
         .then((oldDoc) => {
-          const data = prepareCaseData(caseId, oldDoc);
+          const data = prepareCaseData(solves, caseId, oldDoc);
           writeCaseToFirebase(caseId, data);
         })
         .catch((error) => {
@@ -210,10 +170,6 @@ export default function TestPage(props) {
 
     const caseIds = _.uniqBy(solves, "caseId").map((c) => c.caseId);
     writeCasesToFirebase(caseIds);
-
-    // after updating cases data
-    // update caseSet doc with high level stats
-    // this will be used for the fancy table
   };
 
   return (
