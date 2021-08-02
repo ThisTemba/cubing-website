00a7bb14a6902200c210735f6063ade27ad61356
Temba Mateke <tembamateke@gmail.com>
2021-08-02 18:41:24 -0400

    Show CaseImage, alg and scramble on LearnPage

--- src/components/pages/learnPage.jsx
+++ src/components/pages/learnPage.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Button } from "react-bootstrap";
+import { CaseImage } from "../common/cubing/cubeImage";
+import ScrambleDisplay from "../common/cubing/scrambleDisplay";
+import _ from "lodash";
 
 export default function LearnPage(props) {
+  const currentCase = props.selectedCases[0];
   return (
     <div>
-      <h1>Learn Page</h1>
+      <h1>Learn</h1>
       <Button
         onClick={() => {
           props.history.push("/train");
@@ -16,6 +20,16 @@ export default function LearnPage(props) {
         <i className="fa fa-chevron-left" aria-hidden="true"></i> Back to
         Dashboard
       </Button>
+      <h1>{currentCase.name}</h1>
+      <ScrambleDisplay
+        scramble={"Scramble: " + _.sample(currentCase.scrambles)}
+      />
+      <ScrambleDisplay scramble={"Alg: " + currentCase.algs[0]} />
+      <CaseImage
+        size="200"
+        case={currentCase}
+        caseSetDetails={props.caseSetDetails}
+      />
     </div>
   );
 }
