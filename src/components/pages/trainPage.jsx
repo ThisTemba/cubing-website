5fbfd9a61b2833359c1923c39866e7f69a986ed2
Temba Mateke <tembamateke@gmail.com>
2021-08-02 21:37:56 -0400

    Pass caseSetDetails to LearnPage

--- src/components/pages/trainPage.jsx
+++ src/components/pages/trainPage.jsx
@@ -56,6 +56,7 @@ export default function TrainPage(props) {
             <LearnPage
               onDashboard={() => setDisplayDashboard(true)}
               selectedCases={selectedCases}
+              caseSetDetails={caseSetDetails}
               {...props}
             />
           )}
