7cc40958ae3ff0207d2949e9763480cd3de1faec
Temba Mateke <tembamateke@gmail.com>
2021-08-04 15:54:45 -0400

    Move darkMode side effect into darkmode hook
    This could cause issues down the road, but it works rn

--- src/components/pages/settingsPage.jsx
+++ src/components/pages/settingsPage.jsx
@@ -16,19 +16,6 @@ const SettingsPage = (props) => {
     setDarkMode(!darkMode);
   };
 
-  useEffect(() => {
-    // Source: https://stackoverflow.com/a/19844757/3593621
-    const dark = document.getElementById("dark-mode-css");
-    const light = document.getElementById("light-mode-css");
-    if (darkMode) {
-      dark.rel = "stylesheet";
-      light.rel = "stylesheet alternate";
-    } else {
-      light.rel = "stylesheet";
-      dark.rel = "stylesheet alternate";
-    }
-  }, [darkMode]);
-
   return (
     <Container>
       <h1>Settings</h1>
