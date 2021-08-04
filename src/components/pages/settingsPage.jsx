11225c9357fd45a527bb13c6e199e9875e4315de
Temba Mateke <tembamateke@gmail.com>
2021-08-03 22:04:40 -0400

    Change CSS files based on darkmode

--- src/components/pages/settingsPage.jsx
+++ src/components/pages/settingsPage.jsx
@@ -8,12 +8,26 @@ import Tab from "react-bootstrap/Tab";
 import Button from "react-bootstrap/Button";
 import ListGroup from "react-bootstrap/ListGroup";
 import useDarkMode from "../../hooks/useDarkMode";
+import { useEffect } from "react";
 const SettingsPage = (props) => {
   const [darkMode, setDarkMode] = useDarkMode();
 
   const handleChange = () => {
     setDarkMode(!darkMode);
   };
+
+  useEffect(() => {
+    // Source: https://stackoverflow.com/a/19844757/3593621
+    const dark = document.getElementById("dark-mode-css");
+    const light = document.getElementById("light-mode-css");
+    if (darkMode) {
+      dark.rel = "stylesheet";
+      light.rel = "stylesheet alternate";
+    } else {
+      light.rel = "stylesheet";
+      dark.rel = "stylesheet alternate";
+    }
+  }, [darkMode]);
 
   return (
     <Container>
