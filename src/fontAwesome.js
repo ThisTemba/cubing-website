fb666c45be92139a75139ba1509ae1776589d809
Temba Mateke <tembamateke@gmail.com>
2021-08-06 08:53:54 -0400

    Add fontAwesome.js for importing icons

---
+++ src/fontAwesome.js
@@ -0,0 +1,30 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faCheck,
+  faMinus,
+  faTimes,
+  faSpinner,
+  faTrash,
+  faEye,
+  faEyeSlash,
+  faArrowLeft,
+  faArrowRight,
+  faChevronRight,
+  faChevronLeft,
+  faCaretRight,
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(
+  faCheck,
+  faMinus,
+  faTimes,
+  faSpinner,
+  faTrash,
+  faEye,
+  faEyeSlash,
+  faArrowLeft,
+  faArrowRight,
+  faChevronRight,
+  faChevronLeft,
+  faCaretRight
+);
