3f0ced2789226c3d876cf98f8d197e4fcfe48c5a
Temba Mateke <tembamateke@gmail.com>
2021-08-09 11:03:21 -0400

    Allow create and delete react-select options

--- src/hooks/useCaseModal.jsx
+++ src/hooks/useCaseModal.jsx
@@ -1,14 +1,67 @@
-import { useState } from "react";
-import Select from "react-select";
-import { Table } from "react-bootstrap";
+import { useState, useRef } from "react";
+import { Button, Table } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { components } from "react-select";
+import CreatableSelect from "react-select/creatable";
+import _ from "lodash";
 import useModal from "./useModal";
 import { CaseImage } from "../components/common/cubing/cubeImage";
 
-const CaseModalBody = (props) => {
-  const cas = props.case;
-  const caseSetDetails = props.caseSetDetails;
-  const options = cas.algs.map((a) => ({ value: a, label: a }));
-  const [selectedOption, setSelectedOption] = useState(options[0]);
+const Option = (props) => {
+  const { deletable, value } = props.data;
+  return (
+    <div className="d-flex justify-content-between">
+      <components.Option {...props} />
+      {deletable && (
+        <Button
+          variant="link"
+          className="text-dark"
+          onClick={() => props.onDelete(value)}
+        >
+          <FontAwesomeIcon icon="trash" />
+        </Button>
+      )}
+    </div>
+  );
+};
+
+const CaseModalBody = ({ case: cas, caseSetDetails }) => {
+  const initialOptions = cas.algs.map((a) => ({ value: a, label: a }));
+  const customOption = { value: "!@#$", label: "Custom" };
+  const [options, setOptions] = useState([customOption, ...initialOptions]);
+  const [selectedOption, setSelectedOption] = useState(options[1]);
+  const selectRef = useRef();
+
+  const components = {
+    Option: (props) => <Option {...props} onDelete={handleDelete} />,
+  };
+
+  const handleDelete = (value) => {
+    const newOptions = options.filter((o) => o.value !== value);
+    setOptions(newOptions);
+    const deletedIsSelected = selectedOption.value === value;
+    if (deletedIsSelected) {
+      const i = _.findIndex(options, (o) => o.value === value);
+      if (i !== 0) setSelectedOption(newOptions[i - 1]);
+    }
+  };
+
+  const handleCreate = (inputValue) => {
+    const newOption = { label: inputValue, value: inputValue, deletable: true };
+    setOptions([...options, newOption]);
+    setSelectedOption(newOption);
+  };
+
+  const handleChange = (option) => {
+    if (option && option.value === customOption.value) {
+      setSelectedOption(null);
+      selectRef.current.blur();
+      selectRef.current.focus();
+      // blur then focus ensures blinking typey cursor
+    } else {
+      setSelectedOption(option);
+    }
+  };
 
   return (
     <div className="text-center">
@@ -34,18 +87,27 @@ const CaseModalBody = (props) => {
           </tr>
           <tr>
             <th>{"Algorithm"}</th>
-            <td style={{ width: "350px" }}>
-              <Select
-                value={selectedOption}
-                options={options}
-                width="200px"
-                onChange={(value) => {
-                  setSelectedOption(value);
-                }}
-              />
-            </td>
+            <td>{selectedOption && selectedOption.value}</td>
           </tr>
         </tbody>
+      </Table>
+      <Table bordered>
+        <tr>
+          <th>{"Choose Algorithm:"}</th>
+        </tr>
+        <tr>
+          <td>
+            <CreatableSelect
+              ref={selectRef}
+              options={options}
+              value={selectedOption}
+              onChange={handleChange}
+              components={components}
+              onCreateOption={handleCreate}
+              placeholder="Type or paste custom alg..."
+            />
+          </td>
+        </tr>
       </Table>
     </div>
   );
