4999decfc68be3aeab15d71045ff9287c6d29ed5
Temba Mateke <tembamateke@gmail.com>
2021-08-13 22:08:21 -0400

    Refactor caseStats

--- src/utils/caseStats.js
+++ src/utils/caseStats.js
@@ -36,16 +36,16 @@ const calculateRates = (caseSolves, numSolves) => {
 
 export const getCaseStats = (recentCaseSolves, numSolves, statsCap) => {
   const statCaseSolves = _.take(recentCaseSolves, statsCap);
+  const times = _.map(statCaseSolves, "dur");
   const numStatSolves = statCaseSolves.length;
   const rates = calculateRates(statCaseSolves, numStatSolves);
-  const avgTime = _.mean(statCaseSolves.map((s) => s.dur));
-  const totTime = _.sum(statCaseSolves.map((s) => s.dur));
+  const avgTime = _.mean(times);
   const totAlgLen = _.sum(statCaseSolves.map((s) => getSTM(s.alg)));
   const moveTime = 0.2; // the minimum ammount of time needed to let go of the spacebar and hit it again
   // by excluding this time, TPS is made slightly more accurate
   // without it, longer algorithms tend to have higher TPSes
   // TODO: This is bad for super quick solves
-  const totTimeAdjusted = totTime - numStatSolves * moveTime;
+  const totTimeAdjusted = _.sum(times) - numStatSolves * moveTime;
   const avgTPS = totAlgLen / totTimeAdjusted;
   const caseStats = {
     numSolves,
