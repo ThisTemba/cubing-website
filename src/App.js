3959028bf37491557f10f668fc2e3bf6f1c7813d
Temba Mateke <tembamateke@gmail.com>
2021-08-16 12:13:38 -0400

    Pass {user,userDoc} through UserContext

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+import { useMemo } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
+import { useAuthState, UserContext, usersRef } from "./fire";
+import { useDocumentData } from "react-firebase-hooks/firestore";
 import Navbar from "./components/Navbar";
 import TrainPage from "./components/pages/trainPage";
 import TimePage from "./components/pages/timePage";
@@ -10,21 +13,27 @@ import LogIn from "./components/logIn";
 import useDarkMode from "./hooks/useDarkMode";
 
 function App() {
+  const user = useAuthState();
+  const [userDoc, loading, error] = useDocumentData(usersRef.doc(user?.uid));
+  const userObj = useMemo(() => ({ user, userDoc }), [user, userDoc]);
+
   useDarkMode();
   return (
-    <div className="App">
-      <Navbar />
-      <Switch>
-        <Route path="/time" component={TimePage} />
-        <Route path="/train" component={TrainPage} />
-        <Route path="/stats" component={StatsPage} />
-        <Route path="/signup" component={SignUp} />
-        <Route path="/login" component={LogIn} />
-        <Route path="/settings" component={SettingsPage} />
-        <Route path="/password_reset" component={PasswordReset} />
-        <Redirect path="/" to="/time" />
-      </Switch>
-    </div>
+    <UserContext.Provider value={userObj}>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route path="/time" component={TimePage} />
+          <Route path="/train" component={TrainPage} />
+          <Route path="/stats" component={StatsPage} />
+          <Route path="/signup" component={SignUp} />
+          <Route path="/login" component={LogIn} />
+          <Route path="/settings" component={SettingsPage} />
+          <Route path="/password_reset" component={PasswordReset} />
+          <Redirect path="/" to="/time" />
+        </Switch>
+      </div>
+    </UserContext.Provider>
   );
 }
 
