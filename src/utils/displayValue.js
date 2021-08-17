883bb2f550d65d1f2e069dc965cc21f70b6f68b9
Temba Mateke <tembamateke@gmail.com>
2021-08-17 12:29:19 -0400

    Add displayValue util to disp dur,decimal,overline

---
+++ src/utils/displayValue.js
@@ -0,0 +1,18 @@
+import _ from "lodash";
+import { getTimeString } from "./formatTime";
+
+const undefinedValue = "-";
+
+export const dispDur = (dur) => {
+  if (typeof dur === "undefined") return undefinedValue;
+  return dur === Infinity ? "DNF" : getTimeString(dur * 1000);
+};
+
+export const dispDecimal = (number, n = 2) => {
+  if (typeof number === "undefined") return undefinedValue;
+  return _.round(number, n);
+};
+
+export const dispOverline = (string) => {
+  return <span style={{ textDecoration: "overline" }}>{string}</span>;
+};
