446b14d2980ab10ae2ffd572758925c3a438f51b
Temba Mateke <tembamateke@gmail.com>
2021-08-19 14:35:58 -0400

    Allow external disable FeedbackCard

--- src/components/feedbackCard.jsx
+++ src/components/feedbackCard.jsx
@@ -5,7 +5,12 @@ import _ from "lodash";
 import ButtonGroupToggle from "./common/buttonGroupToggle";
 import useDarkMode from "../hooks/useDarkMode";
 
-export default function FeedbackCard({ currentIndex, solves, setSolves }) {
+export default function FeedbackCard({
+  currentIndex,
+  solves,
+  setSolves,
+  disabled: extDisabled,
+}) {
   const currentSolve = solves[currentIndex];
   const [darkMode] = useDarkMode();
 
@@ -43,7 +48,7 @@ export default function FeedbackCard({ currentIndex, solves, setSolves }) {
   ];
 
   const initial = typeof currentSolve === "undefined";
-  const disabled = initial;
+  const disabled = initial || extDisabled;
   const solveNum = initial ? "#" : solves.length - currentIndex;
   const caseName = initial ? "Case Name" : currentSolve.caseName;
   return (
