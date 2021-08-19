a0a25b74fce0d1b5fe6743a47559ed13aa7be61a
Temba Mateke <tembamateke@gmail.com>
2021-08-19 16:12:11 -0400

    Increase timeDisplay padding on mobile

--- src/components/common/cubing/timeDisplay.jsx
+++ src/components/common/cubing/timeDisplay.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import useWindowDimensions from "../../../hooks/useWindowDimensions";
 import { getTimeString } from "../../../utils/formatTime";
 const TimeDisplay = (props) => {
   let { timeMilliseconds, disabled } = props;
+  const { xs } = useWindowDimensions();
   const displayStyle = {
     fontFamily: "Monospace",
     textAlign: "center",
     fontSize: "100px",
+    padding: xs ? "30px" : "",
   };
   const className =
     props.timerState === "arming"
