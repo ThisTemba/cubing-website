88063f7e8d1fe3496939abb1584797edf8efc7b7
Temba Mateke <tembamateke@gmail.com>
2021-08-19 13:43:44 -0400

    Fix: Blur delete button in FeedbackCard

--- src/components/feedbackCard.jsx
+++ src/components/feedbackCard.jsx
@@ -24,6 +24,7 @@ export default function FeedbackCard({ currentSolve, solves, setSolves }) {
 
   const handleDelete = () => {
     setSolves(solves.length === 1 ? [] : _.tail(solves));
+    document.activeElement.blur();
   };
 
   const mistakesButtons = [
