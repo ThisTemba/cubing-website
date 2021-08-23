8593bdac684820e53027b1b15b5d34db1c2b4d5d
Temba Mateke <tembamateke@gmail.com>
2021-08-23 09:21:49 -0400

    Group OCLL cases by num solved corners

--- src/data/ocllCaseSet.js
+++ src/data/ocllCaseSet.js
@@ -11,7 +11,7 @@ const ocllCaseSet = {
     {
       id: "9c42e369-25cc-4165-bdee-bf1a3b5af6f7",
       name: "OLL 21",
-      group: "",
+      group: "No Corners Solved",
       prob: 2,
       scrambles: [
         "F R2 D2 B2 D' B2 D' R2 F2 U F",
@@ -24,7 +24,7 @@ const ocllCaseSet = {
     {
       id: "1b730f56-333a-4c70-8996-300ba86d0a27",
       name: "OLL 22",
-      group: "",
+      group: "No Corners Solved",
       prob: 4,
       scrambles: [
         "B' U2 B F' U L2 U L2 U' L2 F",
@@ -35,48 +35,9 @@ const ocllCaseSet = {
       algs: ["R U2' R2' U' R2 U' R2' U2' R"],
     },
     {
-      id: "4178d76d-fc48-47af-ab89-76d2e4105db4",
-      name: "OLL 23",
-      group: "",
-      prob: 4,
-      scrambles: [
-        "R U2 R2 F2 D2 L2 D L2 D F2 R",
-        "B' U2 B2 L2 D2 F2 D' F2 D' L2 B'",
-        "B U2 B2 R2 D2 F2 D F2 D R2 B",
-        "F' U2 F2 R2 D2 B2 D' B2 D' R2 F'",
-      ],
-      algs: ["R2 D R' U2 R D' R' U2 R'", "R2 D' R U2 R' D R U2 R"],
-    },
-    {
-      id: "250a4995-086c-4bbb-b01e-8a7afe763230",
-      name: "OLL 24",
-      group: "",
-      prob: 4,
-      scrambles: [
-        "R U R' U' R' F2 R F2 L' U2 L",
-        "B' U' B U F U2 F' L2 B L2 B'",
-        "L' U' L U R U2 R' F2 L F2 L'",
-        "F' U' F U F R2 F' R2 B U2 B'",
-      ],
-      algs: ["r U R' U' r' F R F'", "R U R D R' U' R D' R2"],
-    },
-    {
-      id: "657c744a-12b7-439e-90d4-fea0e0843b09",
-      name: "OLL 25",
-      group: "",
-      prob: 4,
-      scrambles: [
-        "B L2 B' L2 F U2 F' U' B' U B",
-        "B' R2 B R2 F' U2 F U B U' B'",
-        "L' B2 L B2 R' U2 R U L U' L'",
-        "B' U2 B L2 F' L2 F U F U' F'",
-      ],
-      algs: ["x R' U R D' R' U' R D x'", "R' F R B' R' F' R B"],
-    },
-    {
       id: "b2f24506-a6b0-4a6f-96f8-7d959d12c2e8",
       name: "OLL 26",
-      group: "",
+      group: "One Corner Solved",
       prob: 4,
       scrambles: [
         "F' U2 F U2 B U' F' U B' F U",
@@ -89,7 +50,7 @@ const ocllCaseSet = {
     {
       id: "e6547dd9-feea-46ad-b0f5-3abac69170fc",
       name: "OLL 27",
-      group: "",
+      group: "One Corner Solved",
       prob: 4,
       scrambles: [
         "B U2 B' U2 F' U B U' B' F U'",
@@ -98,6 +59,45 @@ const ocllCaseSet = {
         "B U2 B2 U2 B2 U B2 U B2 U' B'",
       ],
       algs: ["R U R' U R U2' R'", "R' U2 R U R' U R"],
+    },
+    {
+      id: "4178d76d-fc48-47af-ab89-76d2e4105db4",
+      name: "OLL 23",
+      group: "Two Corners Solved",
+      prob: 4,
+      scrambles: [
+        "R U2 R2 F2 D2 L2 D L2 D F2 R",
+        "B' U2 B2 L2 D2 F2 D' F2 D' L2 B'",
+        "B U2 B2 R2 D2 F2 D F2 D R2 B",
+        "F' U2 F2 R2 D2 B2 D' B2 D' R2 F'",
+      ],
+      algs: ["R2 D R' U2 R D' R' U2 R'", "R2 D' R U2 R' D R U2 R"],
+    },
+    {
+      id: "250a4995-086c-4bbb-b01e-8a7afe763230",
+      name: "OLL 24",
+      group: "Two Corners Solved",
+      prob: 4,
+      scrambles: [
+        "R U R' U' R' F2 R F2 L' U2 L",
+        "B' U' B U F U2 F' L2 B L2 B'",
+        "L' U' L U R U2 R' F2 L F2 L'",
+        "F' U' F U F R2 F' R2 B U2 B'",
+      ],
+      algs: ["r U R' U' r' F R F'", "R U R D R' U' R D' R2"],
+    },
+    {
+      id: "657c744a-12b7-439e-90d4-fea0e0843b09",
+      name: "OLL 25",
+      group: "Two Corners Solved",
+      prob: 4,
+      scrambles: [
+        "B L2 B' L2 F U2 F' U' B' U B",
+        "B' R2 B R2 F' U2 F U B U' B'",
+        "L' B2 L B2 R' U2 R U L U' L'",
+        "B' U2 B L2 F' L2 F U F U' F'",
+      ],
+      algs: ["x R' U R D' R' U' R D x'", "R' F R B' R' F' R B"],
     },
   ],
 };
