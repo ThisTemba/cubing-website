fc7d65ad97a1afce99c0d39005155f4193ceca61
Temba Mateke <tembamateke@gmail.com>
2021-08-26 13:41:01 -0400

    Refactor sessionsChart

--- src/components/sessionsChart.jsx
+++ src/components/sessionsChart.jsx
@@ -21,14 +21,32 @@ export default function SessionsChart({ statsData }) {
   const getYMax = (dataMax) => {
     return Math.ceil(dataMax / 10) * 10;
   };
+
+  const renderLine = (name, dataKey, stroke) => {
+    const props = { name, dataKey, stroke };
+    return <Line type="monotone" strokeWidth={2} dot={false} {...props} />;
+  };
+
+  const renderLines = () => {
+    return (
+      <>
+        {renderLine("Best Ao100", "bestAo100", "#4285f4")}
+        {renderLine("Best Ao50", "bestAo50", "#db4437")}
+        {renderLine("Best Ao12", "bestAo12", "#f4b400")}
+        {renderLine("Best Ao5", "bestAo5", "#0f9d58")}
+        {renderLine("Best Single", "bestSingle", "#ab47bc")}
+      </>
+    );
+  };
+
   return (
     <div style={{ height: "600px" }}>
       <ResponsiveContainer width="100%" height="100%">
         <LineChart width={500} height={300} data={data} margin={margin}>
           <CartesianGrid strokeDasharray={[3, 3]} />
           <XAxis
-            dataKey="sessionNum"
             type="number"
+            dataKey="sessionNum"
             label={{
               value: "Session Number",
               position: "insideBottomRight",
@@ -37,58 +55,16 @@ export default function SessionsChart({ statsData }) {
             name="session"
           />
           <YAxis
-            dataKey="sessionAverage"
             type="number"
+            dataKey="sessionAverage"
             tickCount={7}
-            allowDecimals={false}
             domain={[0, getYMax]}
             label={{ value: "Time", angle: -90, position: "insideLeft" }}
             unit="s"
           />
-          <ReferenceLine x="Page C" stroke="red" label="Max PV PAGE" />
+          {renderLines()}
 
           <Legend />
-
-          <Line
-            name="Best Ao100"
-            type="monotone"
-            dataKey="bestAo100"
-            strokeWidth={2}
-            stroke="#4285f4"
-            dot={false}
-          />
-          <Line
-            name="Best Ao50"
-            type="monotone"
-            dataKey="bestAo50"
-            strokeWidth={2}
-            stroke="#db4437"
-            dot={false}
-          />
-          <Line
-            name="Best Ao12"
-            type="monotone"
-            dataKey="bestAo12"
-            strokeWidth={2}
-            stroke="#f4b400"
-            dot={false}
-          />
-          <Line
-            name="Best Ao5"
-            type="monotone"
-            dataKey="bestAo5"
-            strokeWidth={2}
-            stroke="#0f9d58"
-            dot={false}
-          />
-          <Line
-            name="Best Single"
-            type="monotone"
-            dataKey="bestSingle"
-            strokeWidth={2}
-            stroke="#ab47bc"
-            dot={false}
-          />
           <Tooltip
             formatter={(value) => value + "s"}
             labelFormatter={(label, other) => {
