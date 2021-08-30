0c369b0a6f56c27b956e0d2848e8a720eb9be0cc
Temba Mateke <tembamateke@gmail.com>
2021-08-30 17:48:14 -0400

    Refactor

--- src/utils/quantiles.js
+++ src/utils/quantiles.js
@@ -12,15 +12,11 @@ const quantile = (arr, q) => {
     return sorted[base];
   }
 };
+export const getP10 = (arr) => quantile(arr, 0.1);
+export const getP90 = (arr) => quantile(arr, 0.95);
 
 export const getQ0 = (arr) => quantile(arr, 0);
-
-export const getP10 = (arr) => quantile(arr, 0.1);
-
 export const getQ1 = (arr) => quantile(arr, 0.25);
 export const getQ2 = (arr) => quantile(arr, 0.5);
 export const getQ3 = (arr) => quantile(arr, 0.75);
-
-export const getP90 = (arr) => quantile(arr, 0.9);
-
 export const getQ4 = (arr) => quantile(arr, 1);
