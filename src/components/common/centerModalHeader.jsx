05ff73b1bc79725f0dfa5771a3c4352275c31c11
Temba Mateke <tembamateke@gmail.com>
2021-08-31 13:19:36 -0400

    Set modalColor based on darkMode
    The default darkMode modal was too dark

--- src/components/common/centerModalHeader.jsx
+++ src/components/common/centerModalHeader.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import Modal from "react-bootstrap/Modal";
 import CloseButton from "react-bootstrap/CloseButton";
 
-export default function CenterModalHeader({ title, onClose }) {
+export default function CenterModalHeader({ title, onClose, style }) {
   return (
-    <Modal.Header>
+    <Modal.Header style={style}>
       <CloseButton disabled style={{ opacity: 0 }} />
       <Modal.Title>{title}</Modal.Title>
       <CloseButton onClick={onClose} />
--- src/hooks/useModal.jsx
+++ src/hooks/useModal.jsx
@@ -1,11 +1,13 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import CenterModalHeader from "../components/common/centerModalHeader";
+import DarkModeContext from "./useDarkMode";
 import _ from "lodash";
 
 export default function useModal() {
   const [show, setShow] = useState(false);
+  const { darkMode } = useContext(DarkModeContext);
   const [content, setContent] = useState({
     title: "Modal Title",
     body: "Modal Body",
@@ -21,13 +23,21 @@ export default function useModal() {
     setShow(false);
   };
 
+  const modalColor = darkMode ? "#212529" : "#f8f9fa";
+
   const ModalComponent = () => {
     if (_.has(content, "title") && _.has(content, "body"))
       return (
         <Modal show={show} onHide={hideModal}>
-          <CenterModalHeader title={content.title} onClose={hideModal} />
-          <Modal.Body>{content.body}</Modal.Body>
-          <Modal.Footer>
+          <CenterModalHeader
+            title={content.title}
+            onClose={hideModal}
+            style={{ background: modalColor }}
+          />
+          <Modal.Body style={{ background: modalColor }}>
+            {content.body}
+          </Modal.Body>
+          <Modal.Footer style={{ background: modalColor }}>
             {content.footer}
             <Button variant="secondary" onClick={hideModal}>
               Close
