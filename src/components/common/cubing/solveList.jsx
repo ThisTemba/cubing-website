39d82894aae868019d62b9590a7bcabda252dc50
Temba Mateke <tembamateke@gmail.com>
2021-09-04 09:56:50 -0400

    Remove obsolete import

--- src/components/common/cubing/solveList.jsx
+++ src/components/common/cubing/solveList.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState, useEffect } from "react";
-import { Table, Button } from "react-bootstrap";
+import { Table } from "react-bootstrap";
 import SimpleBar from "simplebar-react";
 import "simplebar/dist/simplebar.min.css";
 
