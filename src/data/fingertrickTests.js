97288fffcb321ba9d1496d24613d9a2889bba3b1
Temba Mateke <tembamateke@gmail.com>
2021-09-09 14:36:00 -0400

    Comment out a bunch of stuff

--- src/data/fingertrickTests.js
+++ src/data/fingertrickTests.js
@@ -14,82 +14,83 @@ function parseMask(arr, length) {
   return ret;
 }
 
-const parseFingertrick = (ftrick) => {
-  const parsedCode = parseMask(ftrick.code, 3);
+const parseCode = (code) => {
+  const parsedCode = parseMask(code, 3);
   return parsedCode;
 };
 
-const readFingertricks = (fingertricks) => {
-  fingertricks.forEach((ftrick) => parseFingertrick(ftrick));
-};
-
-readFingertricks(fingertricks);
-
-const changeGrip = (grip, move) => {
+const changeGrip = (grip, regrip) => {
   let newGrip;
-  if (grip === "home" && move === "R") newGrip = "R";
-  if (grip === "home" && move === "R'") newGrip = "R'";
-  if (grip === "R" && move === "R'") newGrip = "home";
-  if (grip === "R'" && move === "R") newGrip = "home";
-  if (grip === "R" && move === "R2'") newGrip = "R'";
-  if (grip === "R" && move === "R2") newGrip = "R'";
-  if (grip === "R'" && move === "R2") newGrip = "R";
-  if (grip === "home" && move === "r") newGrip = "R";
-  if (grip === "home" && move === "r'") newGrip = "R'";
-  if (grip === "R" && move === "r'") newGrip = "home";
-  if (grip === "R'" && move === "r") newGrip = "home";
-  if (grip === "R" && move === "r2'") newGrip = "R'";
-  if (grip === "R" && move === "r2") newGrip = "R'";
-  if (grip === "R'" && move === "r2") newGrip = "R";
+  if (grip === "home") {
+    if (regrip === 0x060) newGrip = "R";
+    if (regrip === 0x061) newGrip = "R'";
+  }
+  if (grip === "R") {
+    if (regrip === 0x061) newGrip = "home";
+    if (regrip === 0x063) newGrip = "R'";
+  }
+  if (grip === "R'") {
+    if (regrip === 0x060) newGrip = "home";
+    if (regrip === 0x062) newGrip = "R";
+  }
   newGrip = newGrip || grip;
   //   console.log("grip + move = newGrip,", grip, move, newGrip);
   return newGrip;
 };
 
-const attemptMove = (grip, move) => {
-  //   console.log(grip, move);
-  const code = grips[grip][move]?.[0];
-  if (typeof code === "undefined") {
-    console.log(
-      "\x1b[31m%s\x1b[0m",
-      `could not find ${move} move in ${grip} grip`
-    );
-    return code;
-  }
-  //   console.log(code?.toString(16));
-  const lefty = code > 0xff;
-  const hand = lefty ? "left" : "right";
-  const ftrick = _.find(fingertricks, ["code", lefty ? code - 256 : code]);
-  return code !== null ? hand + " " + ftrick?.description : null;
-};
+// const getFtrick = (grip, move, dataFromPreviousMoves) => {
+//   // given a certain grip, the desired move and some data from previous moves, pick the best fingertrick
+//   const fullCode = grips[grip][move]?.[0];
+//   if (typeof fullCode === "undefined") {
+//     throw new Error(`No data for ${move} move in ${grip} grip`);
+//   }
+//   if (fullCode === null) {
+//     const ftrick = null;
+//     return { code: fullCode, ftrick };
+//   }
+//   const hand = fullCode > 0xff ? 1 : 0;
+//   const fingerMotion = fullCode > 0xff ? fullCode - 0x100 : fullCode;
+//   const ftrick = _.find(fingertricks, ["code", fingerMotion]);
+//   console.log(ftrick);
 
-const attempAlgWithGrip = (initGrip, alg) => {
-  const moves = alg.split(" ");
-  let grip = initGrip;
-  const ftricks = [];
-  moves.forEach((move) => {
-    const attemptedMove = attemptMove(grip, move);
-    grip = changeGrip(grip, move);
-    ftricks.push(attemptedMove);
-  });
-  return ftricks;
-};
+//   return { code: fullCode, ftrick, hand };
+// };
 
-const attemptAlg = (alg) => {
-  const grips = ["home", "R", "R'"];
-  const solutions = [];
-  grips.forEach((grip) => {
-    const ftricks = attempAlgWithGrip(grip, alg);
-    if (!ftricks.includes(null) && !ftricks.includes(undefined)) {
-      solutions.push({ grip, ftricks });
-    }
-  });
-  solutions.forEach(({ ftricks, grip }) => {
-    console.log();
-    console.log(`Basic solution with ${grip} grip:`);
-    ftricks.forEach((a) => console.log(a));
-  });
-  if (solutions.length === 0) console.log("No basic solutions found");
-};
+// const attempAlgWithGrip = (initGrip, alg) => {
+//   const moves = alg.split(" ");
+//   let grip = initGrip;
+//   const ftricks = [];
+//   moves.forEach((move) => {
+//     const { code, ftrick, hand } = getFtrick(grip, move);
+//     grip = changeGrip(grip, ftrick?.regrip);
+//     if (code !== null && code !== undefined) {
+//       ftricks.push(hand ? "left" : "right" + " " + ftrick?.description);
+//       const fullCode = parseCode(ftrick?.code);
+//       fullCode[2] = hand;
+//       const finger = fullCode.slice(1);
+//       console.log(finger);
+//     } else {
+//       ftricks.push(null);
+//     }
+//   });
+//   return ftricks;
+// };
 
-const res = attemptAlg("F R U' R' U' R U R' F' R U R' U' R' F R F'");
+// const attemptAlg = (alg) => {
+//   const grips = ["home", "R", "R'"];
+//   const solutions = [];
+//   grips.forEach((grip) => {
+//     const ftricks = attempAlgWithGrip(grip, alg);
+//     if (!ftricks.includes(null) && !ftricks.includes(undefined)) {
+//       solutions.push({ grip, ftricks });
+//     }
+//   });
+//   solutions.forEach(({ ftricks, grip }) => {
+//     console.log();
+//     console.log(`Basic solution with ${grip} grip:`);
+//     ftricks.forEach((a) => console.log(a));
+//   });
+//   if (solutions.length === 0) console.log("No basic solutions found");
+// };
+
+// const res = attemptAlg("R U R' U' R' F R2 U' R' U' R U R' F'");
