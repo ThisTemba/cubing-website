7f6deb292a95e3810b5ef469078c434f0737782a
Temba Mateke <tembamateke@gmail.com>
2021-09-09 15:30:52 -0400

    All working in new way now
    these commits were messy to start with

--- src/data/fingertrickTests.js
+++ src/data/fingertrickTests.js
@@ -38,59 +38,58 @@ const changeGrip = (grip, regrip) => {
   return newGrip;
 };
 
-// const getFtrick = (grip, move, dataFromPreviousMoves) => {
-//   // given a certain grip, the desired move and some data from previous moves, pick the best fingertrick
-//   const fullCode = grips[grip][move]?.[0];
-//   if (typeof fullCode === "undefined") {
-//     throw new Error(`No data for ${move} move in ${grip} grip`);
-//   }
-//   if (fullCode === null) {
-//     const ftrick = null;
-//     return { code: fullCode, ftrick };
-//   }
-//   const hand = fullCode > 0xff ? 1 : 0;
-//   const fingerMotion = fullCode > 0xff ? fullCode - 0x100 : fullCode;
-//   const ftrick = _.find(fingertricks, ["code", fingerMotion]);
-//   console.log(ftrick);
+const getFtrick = (grip, move) => {
+  // given a certain grip, the desired move and some data from previous moves, pick the best fingertrick
+  const code = grips[grip][move]?.[0];
+  if (typeof code === "undefined") {
+    throw new Error(`No data for ${move} move in ${grip} grip`);
+  } else if (code === null) {
+    // console.log("No good way to do this move with this grip");
+    return { code: null };
+  }
+  const [hand, id] = [(code & 0xf00) / 256, code & 0x0ff]; // split first and last two digits
+  const ftrick = _.find(fingertricks, ["id", id]);
+  return { code, hand, ftrick };
+};
 
-//   return { code: fullCode, ftrick, hand };
-// };
+const attempAlgWithGrip = (initGrip, alg) => {
+  const moves = alg.split(" ");
+  let grip = initGrip;
+  const ftricks = [];
+  moves.forEach((move) => {
+    const { code, hand, ftrick } = getFtrick(grip, move);
+    if (code === null) {
+      ftricks.push(null);
+      return;
+    } else {
+      const regrip = ftrick.regrip ? hand * 256 + ftrick.regrip : null;
+      grip = changeGrip(grip, regrip);
+      const { description0, description1 } = ftrick;
+      const desc = hand === 0 ? description0 : description1 || description0;
+      ftricks.push((hand === 0 ? "right" : "left") + " " + desc);
+    }
+  });
+  return ftricks;
+};
 
-// const attempAlgWithGrip = (initGrip, alg) => {
-//   const moves = alg.split(" ");
-//   let grip = initGrip;
-//   const ftricks = [];
-//   moves.forEach((move) => {
-//     const { code, ftrick, hand } = getFtrick(grip, move);
-//     grip = changeGrip(grip, ftrick?.regrip);
-//     if (code !== null && code !== undefined) {
-//       ftricks.push(hand ? "left" : "right" + " " + ftrick?.description);
-//       const fullCode = parseCode(ftrick?.code);
-//       fullCode[2] = hand;
-//       const finger = fullCode.slice(1);
-//       console.log(finger);
-//     } else {
-//       ftricks.push(null);
-//     }
-//   });
-//   return ftricks;
-// };
+const attemptAlg = (alg, includeInvalid = false) => {
+  const grips = ["home", "R", "R'"];
+  const solutions = [];
+  grips.forEach((grip) => {
+    const ftricks = attempAlgWithGrip(grip, alg);
+    const isValid = !ftricks.includes(null) && !ftricks.includes(undefined);
+    const include = isValid || includeInvalid;
+    if (include) solutions.push({ ftricks, grip });
+  });
+  if (solutions.length > 0) return solutions;
+  else return null;
+};
 
-// const attemptAlg = (alg) => {
-//   const grips = ["home", "R", "R'"];
-//   const solutions = [];
-//   grips.forEach((grip) => {
-//     const ftricks = attempAlgWithGrip(grip, alg);
-//     if (!ftricks.includes(null) && !ftricks.includes(undefined)) {
-//       solutions.push({ grip, ftricks });
-//     }
-//   });
-//   solutions.forEach(({ ftricks, grip }) => {
-//     console.log();
-//     console.log(`Basic solution with ${grip} grip:`);
-//     ftricks.forEach((a) => console.log(a));
-//   });
-//   if (solutions.length === 0) console.log("No basic solutions found");
-// };
+const solutions = attemptAlg("R U' R U R U R U' R' U' R2", true);
+solutions?.forEach(({ ftricks, grip }) => {
+  console.log();
+  console.log(`Basic solution with ${grip} grip:`);
+  ftricks.forEach((a) => console.log(a));
+});
 
-// const res = attemptAlg("R U R' U' R' F R2 U' R' U' R U R' F'");
+if (!solutions) console.log("No solutions for given grips");
