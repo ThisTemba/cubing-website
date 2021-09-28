92bb6bed1c6f1b78248072db05ea363b3294cbc7
Bendik Vedeler <bendik.vedeler@nagra.com>
2021-09-28 11:47:45 +0200

    return '-' from getTimeString if passed a NaN

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -1,11 +1,14 @@
 export const getTimeString = (timeMilliseconds) => {
+  if(isNaN(timeMilliseconds))
+    return '-';
   let cs = Math.floor(timeMilliseconds / 10) % 100; // centiseconds
   let s = Math.floor(timeMilliseconds / 1000) % 60;
   let m = Math.floor(timeMilliseconds / 60000) % 60;
   cs = cs < 10 ? "0" + cs : cs;
-  let timeString = "";
-  if (m === 0) timeString = `${s}.${cs}`;
-  else {
+  let timeString;
+  if (m === 0) {
+    timeString = `${s}.${cs}`;
+  }else {
     s = s < 10 ? "0" + s : s;
     timeString = `${m}:${s}.${cs}`;
   }
